05ff976d783dcdd299b0f9d1a77c342197d14094
A.Yassine <ayassine.auce@gmail.com>
2023-08-03 19:30:30 +0300

    fix map

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,16 @@
 const accounts01 = [['admin01','123'],['admin02','234'],['admin03','345']]
 const accounts02 = {admin01:"123", admin02:"234", admin03:"345"}
-
+let p1 = {name:"ahmad",age:30,state:"beirut"}
+console.log(accounts01)
+console.log(accounts02.keys)
+console.log(accounts02.admin03)
+let admin = prompt("enter username")
 function main(){
-    console.log(accounts01)
-    console.log(accounts02)
+       if(admin in accounts02){
+        console.log(`welcome ${admin} you pass is: ${accounts02[admin]}`)
+       }else{
+        console.log('you are not registered')
+       }
+
 }
+main()
