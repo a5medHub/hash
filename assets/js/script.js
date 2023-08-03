85c8df288548ff3f17c9da3d881eb8c6a9a98eb5
A.Yassine <ayassine.auce@gmail.com>
2023-08-03 18:48:51 +0300

    edit js

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const accounts01 = [['admin01','123'],['admin02','234'],['admin03','345']]
+const accounts02 = {"admin01":"123"}
+
+function main(){
+    console.log(accounts01)
+    console.log(accounts02)
+}
