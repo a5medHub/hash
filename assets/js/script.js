4046e0d03ee3751bd66f32f387905b810241103d
A.Yassine <ayassine.auce@gmail.com>
2023-08-03 20:11:42 +0300

    testing Object.keys

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,40 @@
 const accounts01 = [['admin01','123'],['admin02','234'],['admin03','345']]
 const accounts02 = {admin01:"123", admin02:"234", admin03:"345"}
-let p1 = {name:"ahmad",age:30,state:"beirut"}
-// console.log(accounts01)
-// console.log(accounts02.keys)
-// console.log(accounts02.admin03)
-let admin = prompt("enter username")
-function main(){
-    if(admin in accounts02){
-        console.log(`welcome ${admin} you pass is: ${accounts02[admin]}`)
-    }else{
-        console.log('you are not registered')
+let users = {
+    user01:{
+        name:"ahmad",
+        age:"33",
+        state:"beirut"
+    },
+    user02:{
+        name:'ali', 
+        age:"67"
+    },
+    user03:{
+        name :"mohamed" ,
+        age :  "89" 
+    },
+    user04:{
+        name:"mouad", 
+        age: "12"
     }
 }
+
+// console.log(accounts01)
+// console.log(accounts02.admin03)
+// let admin = prompt("enter username")
+// function main(){
+//     if(admin in accounts02){
+//         console.log(`welcome ${admin} you pass is: ${accounts02[admin]}`)
+//     }else{
+//         console.log('you are not registered')
+//     }
+// }
 function getAge(){
     let name=prompt("enter your name")
-    if(p1.name == name){
-        console.log(`${p1.name} is ${p1.age} years old`)
+    if(users.name == name){
+        console.log(`${users.name} is ${users.age} years old`)
     }else{
-        console.log("wrong name")
+        console.log(`wrong name checkout these names ${users.name}`)
     }
 }
