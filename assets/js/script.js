8dab83c016bbf1f6447bc2ad5d9ea93e34024ef8
A.Yassine <ayassine.auce@gmail.com>
2023-08-08 19:56:28 +0300

    trying new stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,25 @@
-const accounts01 = [['admin01','123'],['admin02','234'],['admin03','345']]
-const accounts02 = {admin01:"123", admin02:"234", admin03:"345"}
-let users = {
-    user01:{
-        name:"ahmad",
-        age:"33",
-        state:"beirut"
-    },
-    user02:{
-        name:'ali', 
-        age:"67"
-    },
-    user03:{
-        name :"mohamed" ,
-        age :  "89" 
-    },
-    user04:{
-        name:"mouad", 
-        age: "12"
-    }
-}
+
+// const accounts01 = [['admin01','123'],['admin02','234'],['admin03','345']]
+// const accounts02 = {admin01:"123", admin02:"234", admin03:"345"}
+// let users = {
+//     user01:{
+//         name:"ahmad",
+//         age:"33",
+//         state:"beirut"
+//     },
+//     user02:{
+//         name:'ali', 
+//         age:"67"
+//     },
+//     user03:{
+//         name :"mohamed" ,
+//         age :  "89" 
+//     },
+//     user04:{
+//         name:"mouad", 
+//         age: "12"
+//     }
+// }
 
 // console.log(accounts01)
 // console.log(accounts02.admin03)
@@ -30,11 +31,11 @@ let users = {
 //         console.log('you are not registered')
 //     }
 // }
-function getAge(){
-    let name=prompt("enter your name")
-    if(users.name == name){
-        console.log(`${users.name} is ${users.age} years old`)
-    }else{
-        console.log(`wrong name checkout these names ${users.name}`)
-    }
-}
+// function getAge(){
+//     let name=prompt("enter your name")
+//     if(users.name == name){
+//         console.log(`${users.name} is ${users.age} years old`)
+//     }else{
+//         console.log(`wrong name checkout these names ${users.name}`)
+//     }
+// }
